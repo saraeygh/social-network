c37f45cc6f2e387c99f961ea2993f0121ec4950a
Reza Saraey <saraeygh@gmail.com>
2023-07-05 14:23:17 +0330

    fix: restrict general foreignkey choices

--- posts/admin.py
+++ posts/admin.py
@@ -1,24 +1,44 @@
 from django.contrib import admin
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.admin import GenericTabularInline
+from useraccounts.models import UserAccount
 from .models import Post, Image, Reply
-from tags.models import TaggedItem
+from tags.models import TaggedItem, Tag
 from reaction.models import Reaction
+
 
 class ImageInLine(admin.TabularInline):
     model = Image
+    exclude = ['soft_delete']
     extra = 0
 
+
 class ReplyInLine(admin.TabularInline):
+    autocomplete_fields = ['user', 'reply_id']
+    exclude = ['soft_delete']
     model = Reply
     extra = 0
 
 
 class TagInLine(GenericTabularInline):
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name == "tag_from":
+            kwargs["queryset"] = ContentType.objects.filter(id=1)            
+        return super(TagInLine, self).formfield_for_foreignkey(db_field, request, **kwargs)
+        
     autocomplete_fields = ['tag']
     model = TaggedItem
     extra = 0
 
+
 class ReactionInLine(GenericTabularInline):
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name == "reaction_from":
+            kwargs["queryset"] = ContentType.objects.filter(id=1)            
+        return super(ReactionInLine, self).formfield_for_foreignkey(db_field, request, **kwargs)
+    
     model = Reaction
     ct_field = "reaction_for"
     extra = 0
@@ -26,15 +46,15 @@ class ReactionInLine(GenericTabularInline):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-   inlines = [ImageInLine, ReplyInLine, TagInLine, ReactionInLine]
-   list_display = ['title', 'user', 'replies_count', 'likes', 'dislike', 'created_at', 'updated_at']
-   search_fields = ['title', 'content']
-   ordering = ['title', 'created_at', 'updated_at']
-   list_per_page = 10
-   prepopulated_fields = {
+    inlines = [ImageInLine, ReplyInLine, TagInLine, ReactionInLine]
+    list_display = ['title', 'user', 'replies_count', 'likes', 'dislike', 'created_at', 'updated_at']
+    search_fields = ['title', 'content']
+    ordering = ['title', 'created_at', 'updated_at']
+    list_per_page = 10
+    prepopulated_fields = {
         "post_slug": ("title",)
         }
-   fieldsets = (
+    fieldsets = (
         ('New post', {
             "fields": (
                 'user',
@@ -44,16 +64,16 @@ class PostAdmin(admin.ModelAdmin):
             ),
         }),
     )
-   exclude = ['soft_delete']
+    exclude = ['soft_delete']
 
-   def replies_count(self, post):
+    def replies_count(self, post):
         return Reply.objects.filter(post_id=post.id).count()
    
-   def likes(self, post):
-       return Reaction.objects.filter(id=post.id).filter(reaction_status='LIKE').count()
+    def likes(self, post):
+        return Reaction.objects.filter(id=post.id).filter(reaction_status='LIKE').count()
    
-   def dislike(self, post):
-       return Reaction.objects.filter(id=post.id).filter(reaction_status='DISLIKE').count()
+    def dislike(self, post):
+        return Reaction.objects.filter(id=post.id).filter(reaction_status='DISLIKE').count()
 
 
 @admin.register(Reply)
--- tags/models.py
+++ tags/models.py
@@ -6,7 +6,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 class TaggedItemManager(models.Manager):
     def get_tags_for(self, obj_type, obj_id):
         content_type = ContentType.objects.get_for_model(obj_type)
-        return TaggedItem.objects.select_related('tag').filter(content_type=content_type, object_id = obj_id)
+        return TaggedItem.objects.select_related('tag').filter(content_type=content_type, object_id=obj_id)
 
 class Tag(models.Model):
     label = models.CharField(
