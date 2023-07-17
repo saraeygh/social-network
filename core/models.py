3d7aa94ed94668e3125a2cf36cf81888dcfb51d0
Reza Saraey <saraeygh@gmail.com>
2023-07-17 19:55:00 +0330

    refact: refactor core app codes

--- core/models.py
+++ core/models.py
@@ -1,50 +1,49 @@
 from django.db import models
-from django.db.models import QuerySet
-from django.db.models.query import QuerySet
 from django.contrib.auth.models import BaseUserManager
-
 from django.utils.translation import gettext_lazy as _
 
 
+# Custome managers
 class AppManager(BaseUserManager):
-    def get_queryset(self) -> QuerySet:
+    def get_queryset(self):
         return super().get_queryset().filter(soft_delete=False)
-    
-    def archived(self) -> QuerySet:
+
+    def archived(self):
         return super().get_queryset().filter(soft_delete=True)
 
+
+# Base model instead of Django default model
 class BaseModel(models.Model, models.Manager):
 
     objects = AppManager()
+    soft_delete = models.BooleanField(
+        default=False,
+        verbose_name=_("Soft delete")
+        )
+
+    def delete(self):
+        self.soft_delete = True
+        self.save()
 
     class Meta:
         abstract = True
-
-    soft_delete = models.BooleanField(default = False)
-    # objects = AppManager()
-
-    
-    def delete(self):
-        """
-        Mark record as soft deleted
-        """
-        self.soft_delete = True
-        self.save()
 
 
 class CreateTimeMixin(models.Model):
     created_at = models.DateTimeField(
-        verbose_name=_("Created at:"),
+        verbose_name=_("Created at"),
         auto_now_add=True
         )
+
     class Meta:
         abstract = True
-    
+
 
 class UpdateTimeMixin(models.Model):
     updated_at = models.DateTimeField(
-        verbose_name=_("Updated at:"),
+        verbose_name=_("Updated at"),
         auto_now=True
         )
+
     class Meta:
         abstract = True
--- core/views.py
+++ core/views.py
@@ -1,26 +1,30 @@
+from typing import Any
+from django import http
 from django.shortcuts import render, redirect
-from urllib.parse import urlencode
 from django.views import View
+
 from posts.models import Post
 from useraccounts.models import UserAccount
 
 
 class LandingPageView(View):
 
+    def setup(self, request, *args, **kwargs):
+        self.posts_list = Post.objects.all()
+        self.users_list = UserAccount.objects.all()
+        self.search_username = request.GET.get("username")
+        return super().setup(request, *args, **kwargs)
+
     def get(self, request):
-        posts_list = Post.objects.all()
-        users_list = UserAccount.objects.all()
-
-        search_username = request.GET.get('username')
-
         context = {
-                    'users_list': users_list,
-                    'posts_list': posts_list,
+                    'users_list': self.users_list,
+                    'posts_list': self.posts_list,
                     'user': request.user,
                 }
 
-        if search_username is None or search_username == '':
+        if self.search_username is None or self.search_username == '':
             return render(request, 'landing.html', context)
-        
-        return redirect(f"useraccounts/search/?username={search_username}")
-        
+
+        return redirect(
+            f"useraccounts/search/?username={self.search_username}"
+            )
